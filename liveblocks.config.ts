22bd823da7bac64916d5d20f42a522167c13433a
Uday Singh <86401007+UdaySinghG70@users.noreply.github.com>
2024-06-10 15:29:42 +0530

    Update liveblocks.config.ts

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -1,66 +1,19 @@
-import {
-  createClient,
-  LiveList,
-  LiveMap,
-  LiveObject,
-} from "@liveblocks/client";
+import { createClient, LiveList, LiveMap, LiveObject } from "@liveblocks/client";
 import { createRoomContext, createLiveblocksContext } from "@liveblocks/react";
-
-import type { Layer, Color } from "@/types/canvas";
+  
+import {Layer, Color} from "@/types/canvas";
 
 const client = createClient({
-  throttle: 16,
-  authEndpoint: "/api/liveblocks-auth",
-  async resolveUsers({ userIds }) {
-    // Used only for Comments and Notifications. Return a list of user information
-    // retrieved from `userIds`. This info is used in comments, mentions etc.
-
-    // const usersData = await __fetchUsersFromDB__(userIds);
-    //
-    // return usersData.map((userData) => ({
-    //   name: userData.name,
-    //   avatar: userData.avatar.src,
-    // }));
-
-    return [];
-  },
-  async resolveMentionSuggestions({ text }) {
-    // Used only for Comments. Return a list of userIds that match `text`.
-    // These userIds are used to create a mention list when typing in the
-    // composer.
-    //
-    // For example when you type "@jo", `text` will be `"jo"`, and
-    // you should to return an array with John and Joanna's userIds:
-    // ["john@example.com", "joanna@example.com"]
-
-    // const users = await getUsers({ search: text });
-    // return users.map((user) => user.id);
-
-    return [];
-  },
-  async resolveRoomsInfo({ roomIds }) {
-    // Used only for Comments and Notifications. Return a list of room information
-    // retrieved from `roomIds`.
-
-    // const roomsData = await __fetchRoomsFromDB__(roomIds);
-    //
-    // return roomsData.map((roomData) => ({
-    //   name: roomData.name,
-    //   url: roomData.url,
-    // }));
-
-    return [];
-  },
+  throttle:16,
+  authEndpoint:"/api/liveblocks-auth",
 });
 
-// Presence represents the properties that exist on every user in the Room
-// and that will automatically be kept in sync. Accessible through the
-// `user.presence` property. Must be JSON-serializable.
+
 type Presence = {
-  cursor: { x: number; y: number } | null;
-  selection: string[];
-  pencilDraft: [x: number, y: number, pressure: number][] | null;
-  penColor: Color | null;
+  cursor:{x:number, y:number} | null,
+  selection:string[];
+  pencilDraft:[x:number,y:number,pressure:number][]|null;
+  penColor:Color | null;
 };
 
 // Optionally, Storage represents the shared document that persists in the
@@ -79,7 +32,7 @@ type UserMeta = {
   id?: string;
   info?: {
     name?: string;
-    picture?: string;
+    picture?:string;
   };
 };
 
@@ -139,14 +92,12 @@ export const {
     useMarkThreadAsRead,
     useRoomNotificationSettings,
     useUpdateRoomNotificationSettings,
-
+  
     // These hooks can be exported from either context
     // useUser,
     // useRoomInfo
-  },
-} = createRoomContext<Presence, Storage, UserMeta, RoomEvent, ThreadMetadata>(
-  client,
-);
+  }
+} = createRoomContext<Presence, Storage, UserMeta, RoomEvent, ThreadMetadata>(client);
 
 // Project-level hooks, use inside `LiveblocksProvider`
 export const {
@@ -156,9 +107,9 @@ export const {
     useMarkAllInboxNotificationsAsRead,
     useInboxNotifications,
     useUnreadInboxNotificationsCount,
-
+  
     // These hooks can be exported from either context
     useUser,
     useRoomInfo,
-  },
+  }
 } = createLiveblocksContext<UserMeta, ThreadMetadata>(client);
